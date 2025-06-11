7faa4891cfc3a330e430e7f1baebf7f17213a4f4
KOmoROid <135452272+KOmoROid@users.noreply.github.com>
2025-06-11 00:33:38 -0300

    Revert "push"
    This reverts commit 16b284669b8060c499e2f4c12abec14c72e55974.

--- ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Mapper/ArticuloManufacturadoMapper.java
+++ ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Mapper/ArticuloManufacturadoMapper.java
@@ -3,21 +3,14 @@ package ElBuenSabor.ProyectoFinal.Mapper;
 import ElBuenSabor.ProyectoFinal.DTO.ArticuloManufacturadoDTO;
 import ElBuenSabor.ProyectoFinal.Entities.ArticuloManufacturado;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ArticuloManufacturadoMapper extends BaseMapper <ArticuloManufacturado, ArticuloManufacturadoDTO>{
 
-    @Override
-    @Mapping(target = "detalles", source = "articuloManufacturadoDetalles") // Mapea 'articuloManufacturadoDetalles' del DTO a 'detalles' de la entidad
-    ArticuloManufacturado toEntity(ArticuloManufacturadoDTO dto);
-
-    @Override
-    @Mapping(target = "articuloManufacturadoDetalles", source = "detalles") // Mapea 'detalles' de la entidad a 'articuloManufacturadoDetalles' del DTO
-    ArticuloManufacturadoDTO toDTO(ArticuloManufacturado entity);
+    ArticuloManufacturadoDTO articuloManufacturadoToArticuloManufacturadoDto(ArticuloManufacturado articuloManufacturado);
+    ArticuloManufacturado articuloManufacturadoDtoToArticuloManufacturado(ArticuloManufacturadoDTO articuloManufacturadoDTO);
 
     List<ArticuloManufacturadoDTO> amToAmDto(List<ArticuloManufacturado> articulosManufacturados);
     List<ArticuloManufacturado> amDtoToAm(List<ArticuloManufacturadoDTO> articulosManufacturadosDTO);

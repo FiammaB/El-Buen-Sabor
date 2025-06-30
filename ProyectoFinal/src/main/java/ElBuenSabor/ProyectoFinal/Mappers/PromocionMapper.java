f39347e7e26dcbffcd99705f5f4da5bf5d011d58
Fiamma B <brizuelafiamma6@gmail.com>
2025-06-30 10:33:35 -0300

    ahora se ven la img de promociones

--- ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Mappers/PromocionMapper.java
+++ ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Mappers/PromocionMapper.java
@@ -4,16 +4,20 @@ import ElBuenSabor.ProyectoFinal.DTO.PromocionCreateDTO;
 import ElBuenSabor.ProyectoFinal.DTO.PromocionDTO;
 import ElBuenSabor.ProyectoFinal.Entities.Promocion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {ImagenMapper.class})
 public interface PromocionMapper {
 
+    // Este método ahora mapeará la imagen automáticamente gracias a 'uses = {ImagenMapper.class}'
     PromocionDTO toDTO(Promocion entity);
 
     Promocion toEntity(PromocionDTO dto);
 
+    @Mapping(target = "articulosManufacturados", ignore = true)
+    @Mapping(target = "sucursales", ignore = true)
     Promocion toEntity(PromocionCreateDTO dto);
 
     List<PromocionDTO> toDTOList(List<Promocion> promociones);

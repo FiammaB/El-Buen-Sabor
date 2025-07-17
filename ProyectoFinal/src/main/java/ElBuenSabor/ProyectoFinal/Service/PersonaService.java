81caa465c26f654e386b079742ecab3fd86e2df8
Laura Pelayes Cabrera <32913954+LauraPelayes@users.noreply.github.com>
2025-07-17 20:30:06 -0300

    solo se puede editar activo y teléfono del cliente del lado del admin

--- ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Service/PersonaService.java
+++ ProyectoFinal/src/main/java/ElBuenSabor/ProyectoFinal/Service/PersonaService.java
@@ -1,7 +1,9 @@
 package ElBuenSabor.ProyectoFinal.Service;
 
 import ElBuenSabor.ProyectoFinal.Entities.Persona;
+import ElBuenSabor.ProyectoFinal.DTO.ClienteAdminUpdateDTO; // <-- AÑADIR IMPORTACIÓN
 
 public interface PersonaService extends BaseService<Persona, Long>{
+    void actualizarClienteDesdeAdmin(Long personaId, ClienteAdminUpdateDTO dto) throws Exception;
 
 }
